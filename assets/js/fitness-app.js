34c3667f5b3d3a913de68f03ed87592da1bca6a8
Sofronijev <sofronijev@gmail.com>
2019-09-22 18:57:54 +0200

    application finished

--- assets/js/fitness-app.js
+++ assets/js/fitness-app.js
@@ -1,4 +1,24 @@
 //get all information when app starts
 showHomeScreen();
-//showDetailedInfo();
+
+document.getElementById("week").addEventListener("click", function (ev) {
+    let clickedElement = ev.target;
+    if (clickedElement.hasAttribute("data-date")) {
+        let date = clickedElement.getAttribute("data-date");
+        showDetailedInfo(date);
+        let days = document.querySelectorAll(".day");
+        days.forEach(day => {
+            day.classList.remove("dayActive");
+        })
+        clickedElement.classList.add("dayActive");
+    }
+});
+
+document.getElementById("backBtn").addEventListener("click", function () {
+    let days = document.querySelectorAll(".day");
+    days.forEach(day => {
+        day.classList.remove("dayActive");
+    })
+    showHomeScreen();
+});
 
